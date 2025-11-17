6acf432752727f5728ddbcb7e41a6cb0b4178939
oooooming <minjio030107@ewhain.net>
2025-11-17 18:52:51 +0900

    주석 처리 추가

--- src/main/java/com/efub/SpringBatch/entity/BeforeEntity.java
+++ src/main/java/com/efub/SpringBatch/entity/BeforeEntity.java
@@ -1,12 +1,12 @@
-package com.efub.SpringBatch.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-
-public class BeforeEntity {
-
-}
+//package com.efub.SpringBatch.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//public class BeforeEntity {
+//
+//}
--- src/main/java/com/efub/SpringBatch/repository/BeforeRepository.java
+++ src/main/java/com/efub/SpringBatch/repository/BeforeRepository.java
@@ -1,7 +1,7 @@
-package com.efub.SpringBatch.repository;
-
-import com.efub.SpringBatch.entity.BeforeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
-}
+//package com.efub.SpringBatch.repository;
+//
+//import com.efub.SpringBatch.entity.BeforeEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+//}
